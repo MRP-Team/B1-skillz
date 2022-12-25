422c2360c49b47d4aafd8e380c3af6092fcb7410
abdel1touimi <abdel1.touimi@gmail.com>
2022-12-25 22:23:48 +0100

    levels fix

--- config.lua
+++ config.lua
@@ -18,49 +18,49 @@ Config.ShootingSkillUpAmount = 0.1 --the amount the skill goes up when triggered
 Config.Skills = {
     ["Stamina"] = {
         ["Current"] = 0, -- Default value
-        ["RemoveAmount"] = -0.3, -- % to remove when updating,
+        ["RemoveAmount"] = -1, -- % to remove when updating,
         ["Stat"] = "MP0_STAMINA", -- GTA stat hashname
         ['icon'] = 'fas fa-walking',
     },
 
     ["Strength"] = {
         ["Current"] = 0,
-        ["RemoveAmount"] = -0.3,
+        ["RemoveAmount"] = -1,
         ["Stat"] = "MP0_STRENGTH",
         ['icon'] = 'fas fa-dumbbell',
     },
 
     ["Lung Capacity"] = {
         ["Current"] = 0,
-        ["RemoveAmount"] = -0.1,
+        ["RemoveAmount"] = -1,
         ["Stat"] = "MP0_LUNG_CAPACITY",
         ['icon'] = 'fas fa-heartbeat',
     },
 
     ["Shooting"] = {
         ["Current"] = 0,
-        ["RemoveAmount"] = -0.1,
+        ["RemoveAmount"] = -1,
         ["Stat"] = "MP0_SHOOTING_ABILITY",
         ['icon'] = 'fas fa-bullseye',
     },
 
     ["Driving"] = {
         ["Current"] = 0,
-        ["RemoveAmount"] = -0.5,
+        ["RemoveAmount"] = -1,
         ["Stat"] = "MP0_DRIVING_ABILITY",
         ['icon'] = 'fas fa-car-side',
     },
 
     ["Wheelie"] = {
         ["Current"] = 0,
-        ["RemoveAmount"] = -0.2,
+        ["RemoveAmount"] = -1,
         ["Stat"] = "MP0_WHEELIE_ABILITY",
         ['icon'] = 'fas fa-wheelchair',
     },
 
     ["Fishing"] = {
         ["Current"] = 0,
-        ["RemoveAmount"] = 0,
+        ["RemoveAmount"] = -1,
         ['icon'] = 'fas fa-fish-fins',
     }
 }
